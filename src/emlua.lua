fd87b71775f495ecc1e6470f6e28baee43dbe283
ysugimoto <sugimoto@wnotes.net>
2018-10-11 19:51:49 +0900

    single file

--- src/emlua.lua
+++ src/emlua.lua
@@ -1,7 +1,81 @@
-package.path = package.path .. ';/opt/emlua/?.lua'
+-- last_index finds last index of substring is found
+-- @param string str - source string
+-- @param string substring - find substring
+-- @return int pos - index number. if substring is not found, returns -1
+local function last_index(str, substring)
+  local pos = string.find(str, substring, 1, true)
+  if not pos then
+    return -1
+  end
 
-local fs = require('filesystem')
+  while true do
+    new_pos = string.find(str, substring, pos + 1, true)
+    if not new_pos then
+      break
+    end
+    pos = new_pos
+  end
 
+  return pos
+end
+
+-- Convert string to hex array
+-- @param string buffer
+-- @return table hex
+local function to_hex(buffer)
+  local hex = {}
+  for c in buffer:gmatch('.') do
+    table.insert(hex, ('0x%02x'):format(string.byte(c)))
+  end
+  return hex
+end
+
+-- Get file basename
+-- @param string path
+-- @return string
+local function basename(path)
+  local basename = path
+  local slash = last_index(basename, '/')
+  if slash > -1 then
+    basename = string.sub(basename, slash + 1)
+  end
+  local dot = last_index(basename, '.')
+  if dot > -1 then
+    basename = string.sub(basename, 1, dot - 1)
+  end
+  return basename
+end
+
+-- Check file existence
+-- @param string path
+-- @return bool - true if file exists
+local function file_exists(path)
+  local fp = io.open(path, 'r')
+  if fp then
+    fp:close()
+    return true
+  end
+  return false
+end
+
+-- Read file contents as hex table
+-- @param string path
+-- @return table
+local function read_as_hex(path)
+  local fp = io.open(path, 'r')
+  local buffer = fp:read('*all')
+  local ok = fp:close()
+  if ok then
+    return to_hex(buffer)
+  end
+  return {}
+end
+
+-----------------------------------------------------------
+-- Main process
+-----------------------------------------------------------
+
+-- Generate C file template
 local c_template = [[
 #ifdef __cplusplus
 extern "C" {
@@ -23,6 +97,8 @@ extern "C" {
 %s
 ]]
 
+-- Geneare wasm function string
+-- @param string file
 local function genearte_wasm_function(file)
   local wasm_function = [[
 EMSCRIPTEN_KEEPALIVE
@@ -42,39 +118,40 @@ EMSCRIPTEN_KEEPALIVE
 ]]
   return string.format(
     wasm_function,
-    'const char*',
-    file.basename,
-    '',
-    table.concat(file:readHex(), ', '),
+    'const char*', -- TODO: support other return types
+    basename(file),
+    '', -- TODO: support arguments
+    table.concat(read_as_hex(file), ', '),
     ''
   )
 end
 
-local lua_files = {}
-for _, name in ipairs(arg) do
-  local file = fs.new(name)
-  if file:exists() then
-    table.insert(lua_files, file)
+-- Main function
+local function main(args)
+
+  -- Collect exist lua files
+  local lua_files = {}
+  for _, file in ipairs(args) do
+    if file_exists(file) then
+      table.insert(lua_files, file)
+    end
   end
+
+  if #lua_files == 0 then
+    print("No target files.")
+    os.exit(1)
+  end
+
+  -- Geneate wasm function codes
+  local wasm_functions = {}
+  for _, f in ipairs(lua_files) do
+    table.insert(wasm_functions, genearte_wasm_function(f))
+  end
+
+  -- output C file to compile
+  local source = io.open('./emlua.c', 'w')
+  source:write(string.format(c_template, table.concat(wasm_functions, "\n")))
+  source:close()
 end
 
-if #lua_files == 0 then
-  print("No target files.")
-  os.exist(1)
-end
-
-local wasm_functions = {}
-for _, f in ipairs(lua_files) do
-  table.insert(wasm_functions, genearte_wasm_function(f))
-end
-
--- output C file to compile
-local source = io.open('./emlua.c', 'w')
-source:write(string.format(c_template, table.concat(wasm_functions, "\n")))
-source:close()
-
-
-
-
-
-
+main(arg)
